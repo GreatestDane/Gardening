223de0be9d6bfcbd2a0e19ed44931675ff66af05
Greatest Dane <DaneGoodman85@gmail.com>
2019-09-21 15:23:02 -0700

    Loaded Bootstrap and other dependancies

--- gardening-react/src/components/Landing/Landing.js
+++ gardening-react/src/components/Landing/Landing.js
@@ -3,7 +3,7 @@ import "./Landing.css";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
-library.add(faStroopwafel)
+library.add(faStroopwafel, FontAwesomeIcon)
 
 
 const Landing = () => {
