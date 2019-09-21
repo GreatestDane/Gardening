996c946911e6010dc20ce28aed5243c989d814db
Greatest Dane <DaneGoodman85@gmail.com>
2019-09-21 14:12:57 -0700

    I am attempting to push up a modified version of the RENTR landing page

--- gardening-react/src/App.js
+++ gardening-react/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          ALSO TURN BACK NOW BECAUSE YOU KNOW NOT WHAT YOU DO
         </p>
         <a
           className="App-link"
@@ -23,4 +24,4 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
---
+++ gardening-react/src/components/Landing.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Landing.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+library.add(faStroopwafel)
+
+
+const Landing = () => {
+    return (
+        <div className="landing-box container-fluid">
+            <div id="landing-div" className="jumbotron bg-transparent rounded-0">
+                <h1 id="landing-title">T H E  G A R D E N S</h1>
+                <div className="text-center">
+                    <a href="/login" className="top-button btn-lg m-1 btn btn-primary">Login</a>
+                    <a href="/register" className="top-button btn-lg m-1 btn btn-primary">Register</a>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Landing;
